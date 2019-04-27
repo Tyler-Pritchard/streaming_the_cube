080679bd7c0731c9f4c2b93f3d0f454d2cd3f3b5
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-27 11:55:31 -0700

    add browserrouter and page 1/2

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
