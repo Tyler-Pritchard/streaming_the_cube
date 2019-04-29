79bdd47e8c4ef088a805db2fa985a465b3919eac
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-28 22:52:21 -0700

    import streams

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
