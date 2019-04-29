25a30e6fc608123b65347d1287493b03f191fa9e
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-28 22:50:21 -0700

    delete page 1 & 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,31 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
-
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
-      </BrowserRouter>
+      <BrowserRouter />
     </div>
   );
 };
