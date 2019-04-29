97bb52cb92836cea634faa7f611e4472edb177be
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-28 22:56:17 -0700

    add rout paths to browserrouter

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,15 @@ import StreamShow from "./streams/StreamShow";
 const App = () => {
   return (
     <div>
-      <BrowserRouter />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
