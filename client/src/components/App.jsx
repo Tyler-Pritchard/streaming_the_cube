39dacd7c9c73cc8029cace92bbe2926686d551d7
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-28 23:46:05 -0700

    header

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,11 +5,12 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
-      <h1>Header!</h1>
+      <Header />
       <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
