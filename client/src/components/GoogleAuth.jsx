12f6760d8e3649e85b3b6b96a0860d24e1a1046c
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-29 22:59:34 -0700

    add componentDidMount to GAuth

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2");
+  }
+
   render() {
     return <div>Google Auth</div>;
   }
