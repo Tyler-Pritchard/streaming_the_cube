f1797bf0f86d16d62ebedda29794804501ff3d04
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-30 22:01:43 -0700

    add .then this.auth to mount method

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -3,11 +3,15 @@ import React from "react";
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "736430770142-1r3g0la18unvr2jqapcn7rc20vmli166.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "736430770142-1r3g0la18unvr2jqapcn7rc20vmli166.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+        });
     });
   }
 
