62fd954c884ecb19dc1fa0b18661f5ec9573c29c
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-30 22:09:41 -0700

    add dummy renderAuthButton function

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -18,6 +18,16 @@ class GoogleAuth extends React.Component {
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
     return <div>Google Auth</div>;
   }
