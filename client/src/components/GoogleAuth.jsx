f59ddae082cd38c73607b283062d8c8edb58efa4
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-02 21:51:31 -0700

    add mapstatetoprops to GAuth

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -62,6 +62,10 @@ class GoogleAuth extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
 export default connect(
   null,
   { signIn, signOut }
