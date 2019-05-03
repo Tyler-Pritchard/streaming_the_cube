01782200eda83db8a800385538114defff8d7ecf
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-02 21:46:51 -0700

    add authReducers, state & switch cases

---
+++ client/src/reducers/authReducers.jsx
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  isSignedIn: null
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SIGN_IN":
+
+    case "SIGN_OUT":
+
+    default:
+      return state;
+  }
+};
