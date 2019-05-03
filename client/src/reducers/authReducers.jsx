023b69a398249a86bdc81e8e26522b809a6ef50d
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-02 21:49:12 -0700

    import auth reducer to reducers index

--- client/src/reducers/authReducers.jsx
+++ client/src/reducers/authReducers.jsx
@@ -5,9 +5,9 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "SIGN_IN":
-
+      return { ...state, isSignedIn: true };
     case "SIGN_OUT":
-
+      return { ...state, isSignedIn: false };
     default:
       return state;
   }
