1c4734a24187fc2073721a132c3f86d8f6820b5a
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-02 21:04:59 -0700

    clarify OAuth handles

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -19,11 +19,11 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
+  onAuthChangeClick = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
@@ -36,14 +36,14 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
           Sign In with Google
         </button>
