e5cdd60f73107968d86d2601e77b137a3b0b4469
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-02 20:59:20 -0700

    add sign in/out functions

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -23,6 +23,14 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
