776c9fb56a5c0a51d128d95cf40f00998f37054d
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-04 23:05:50 -0700

    add form validate funtions

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -26,10 +26,25 @@ class StreamCreate extends React.Component {
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: "streamCreate"
