5a8228c15cf6d444b97d57de64f7c248c9d328ed
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-04 22:41:22 -0700

    add form label

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -3,13 +3,18 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
   renderInput({ input }) {
-    return <input {...input} />;
+    return (
+      <div className="field">
+        <label />
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderInput} />
+        <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} />
       </form>
     );
