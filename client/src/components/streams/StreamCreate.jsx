c65b318830ae5838bf22934b911e8e76a9a9f69c
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-05 22:36:11 -0700

    add renderError to streamcreate

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
   renderInput({ input, label, meta }) {
-    console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
   }
