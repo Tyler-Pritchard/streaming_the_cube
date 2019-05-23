d0c193927dbf088f107c11e8b8b82c17c950d276
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-23 11:12:44 -0700

    typo in action index

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -16,7 +16,7 @@ export const signOut = () => {
 
 export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
-  const response = await streams.post("/streams", ...formValues, userId);
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
