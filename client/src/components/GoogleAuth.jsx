8d10123895aa66c9f04245ece0e69781dd4dae1e
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-23 10:48:30 -0700

    OAuth API key replaced

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -8,7 +8,7 @@ class GoogleAuth extends React.Component {
       window.gapi.client
         .init({
           clientId:
-            "736430770142-1r3g0la18unvr2jqapcn7rc20vmli166.apps.googleusercontent.com",
+            "329396094838-5um4vnlocaf4nijtbm1bmtre42an18t7.apps.googleusercontent.com",
           scope: "email"
         })
         .then(() => {
