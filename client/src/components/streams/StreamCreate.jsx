186f453dd4c442d1d65e859904c667cae8fc2188
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-27 12:45:34 -0700

    found typo

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStream } from "../../actions";
-import StreamForm from "./StreamForm";
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  onSubmit = formValues => {
-    this.props.createStream(formValues);
-  };
+  renderInput() {
+    return <div>I'm an input!</div>;
+  }
 
   render() {
     return (
-      <div>
-        <h3>Create a Stream</h3>
-        <StreamForm onSubmit={this.onSubmit} />
-      </div>
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
     );
   }
 }
 
-export default connect(
-  null,
-  { createStream }
-)(StreamCreate);
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
