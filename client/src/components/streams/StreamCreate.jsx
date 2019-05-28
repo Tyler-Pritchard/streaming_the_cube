b21e5df31cf2956acbc7a3db40ab5b790524caa1
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-27 22:23:56 -0700

    add submit button to create streams

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -11,15 +11,21 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {}
+
   render() {
     return (
-      <form>
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
           name="description"
           component={this.renderInput}
           label="Enter Description"
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
