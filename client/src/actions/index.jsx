cfaff7a9080e9c88f59925895769619637b49d5d
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-30 20:37:54 -0700

    assign method CREATE_STREAM

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -15,5 +15,7 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
