58791d1e379805cf3505d761cc46a2ef0dfab862
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-03 16:29:46 -0700

    render edit/del buttons for admin

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -7,6 +7,12 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return <div>EDIT/DELETE</div>;
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -16,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -32,7 +39,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
