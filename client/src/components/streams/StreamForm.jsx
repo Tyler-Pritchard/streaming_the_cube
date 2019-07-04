7b72b72b1b648600628c8a7d69add9c1d5a7568b
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-03 19:23:33 -0700

    boilerplate set up

--- client/src/components/streams/StreamForm.jsx
+++ client/src/components/streams/StreamForm.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { connect } from "react-redux";
-import { createStream } from "../../actions";
 
-class StreamCreate extends React.Component {
+class StreamForm extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
       return (
@@ -27,7 +25,7 @@ class StreamCreate extends React.Component {
   };
 
   onSubmit = formValues => {
-    this.props.createStream(formValues);
+    this.props.onSubmit(formValues);
   };
 
   render() {
@@ -59,12 +57,7 @@ const validate = formValues => {
   return errors;
 };
 
-const formWrapped = reduxForm({
-  form: "streamCreate",
+export default reduxForm({
+  form: "streamForm",
   validate
-})(StreamCreate);
-
-export default connect(
-  null,
-  { createStream }
-)(formWrapped);
+})(StreamForm);
