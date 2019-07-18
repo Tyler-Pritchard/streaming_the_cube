780c6451c48cc2528394ec75872f2851a3e5fb70
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-17 21:14:30 -0700

    add buildPlayer functionality

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -14,10 +14,25 @@ class StreamShow extends React.Component {
     const { id } = this.props.match.params;
 
     this.props.fetchStream(id);
-    flv.createPlayer({
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`
     });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
