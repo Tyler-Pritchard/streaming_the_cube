a2a1a209d31ab6df52a0c6ed555044b5bcfa27af
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-17 20:58:48 -0700

    add stream input id

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -11,7 +11,13 @@ class StreamShow extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
   }
 
   render() {
@@ -23,7 +29,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video />
+        <video ref={this.videoRef} style={{ width: "100% " }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
